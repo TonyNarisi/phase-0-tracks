0bb0b47d0c2201ebe48c27cfc9a74a6555eb9cb0
Tony Narisi <tonynarisi@gmail.com>
2016-05-31 21:08:21 -0400

    Write pseudocode for method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+# Pseudocode
+# Split the full name string into an array to separate first and last names
+# Reverse the position of the names
+# Join the names back together
+# Make all letters in the full name lowercase so that they are easier to smoothly manipulate
+# Define string of all vowels and string of all consonants
+# Loop through the string, changing all vowels to next vowel and all consonants to next consonant
+#   Use previously defined strings to change these
+#   Define special conditions for edge cases
+# Capitalize the first letter of the first name and of the last name
+
+def 
