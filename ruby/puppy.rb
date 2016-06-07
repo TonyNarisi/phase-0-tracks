eb8584a6e58815778425d315414884ab9c781a9c
Tony Narisi <tonynarisi@gmail.com>
2016-06-06 21:43:10 -0400

    Pseudocode puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+# CHARACTERISTICS
+
+# Fluffy
+# Cute
+# Energetic
+# Playful
+
+# BEHAVIORS
+
+# Bark
+# Run
+# Sleep
+# Eat
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
