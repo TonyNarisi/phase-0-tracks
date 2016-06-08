4045a3015223f36af3b699afc0a4caabbdc38c03
Tony Narisi <tonynarisi@gmail.com>
2016-06-07 23:04:45 -0400

    Add random Santa generator and organize driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,8 @@
 class Santa
 
-  attr_reader :age, :ethnicity
+  attr_reader :ethnicity
 
-  attr_accessor :gender
+  attr_accessor :gender, :age
 
   def initialize(gender, ethnicity)
     p "Initializing Santa instance..."
@@ -33,20 +33,40 @@ end
 
 # DRIVER CODE
 
+# Release 0
+
 # claude = Santa.new
 # claude.speak
 # claude.eat_milk_and_cookies("chocolate chip cookie")
 
-santas = []
-santa_genders = ["transgender", "androgynous", "gender questioning", "female", "pangender"]
-santa_ethnicities = ["Native American", "Pacific Islander", "African American", "Asian", "Latino"]
-santa_genders.length.times { |i| santas << Santa.new(santa_genders[i], santa_ethnicities[i]) }
+# Release 1
 
-p "#{santas[0].age}"
-santas[0].celebrate_birthday
-p "#{santas[0].age}"
-p "#{santas[0].ethnicity}"
-santas[0].gender = "androgynous"
-santas[0].get_mad_at("Dasher")
+# santas = []
+# santa_genders = ["transgender", "androgynous", "gender questioning", "female", "pangender"]
+# santa_ethnicities = ["Native American", "Pacific Islander", "African American", "Asian", "Latino"]
 
-p santas
+# santa_genders.length.times { |i| santas << Santa.new(santa_genders[i], santa_ethnicities[i]) }
+
+# Release 2
+
+# p "#{santas[0].age}"
+# santas[0].celebrate_birthday
+# p "#{santas[0].age}"
+# p "#{santas[0].ethnicity}"
+# santas[0].gender = "androgynous"
+# santas[0].get_mad_at("Dasher")
+
+# p santas
+
+# Release 4
+
+santa_genders = ["transgender", "androgynous", "gender questioning", "female", "pangender", "male", "agender", "bigender"]
+santa_ethnicities = ["Native American", "Pacific Islander", "African American", "Asian", "Latino", "white", "black", "Japanese-African"]
+
+100.times do
+  santa = Santa.new(santa_genders.sample, santa_ethnicities.sample)
+  santa.age = rand(140)
+  p santa.gender
+  p santa.ethnicity
+  p santa.age
+end
