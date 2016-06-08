2f17c8e26d51180351717e3bf06eb1dacaa614e8
Tony Narisi <tonynarisi@gmail.com>
2016-06-07 21:50:23 -0400

    Add methods to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,21 @@
 class Santa
 
+  def initialize
+    p "Initializing Santa instance..."
+  end
+
   def speak
-    
+    p "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    p "That was a good #{cookie_type}!"
   end
 
 end
+
+# DRIVER CODE
+
+claude = Santa.new
+claude.speak
+claude.eat_milk_and_cookies("chocolate chip cookie")
