7bd57e6ead0a4a888b7ed53d5ce1c906af8e482c
Tony Narisi <tonynarisi@gmail.com>
2016-06-07 20:16:34 -0400

    Add methods into Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    p "Woof!" * num.to_i
+  end
+
+  def roll_over
+    p "*rolls over*"
+  end
+
+  def dog_years(num)
+    p num.to_i * 7
+  end
+
+  def shake_hands(num)
+    p "*puts paw out*" * num.to_i
+  end
+
 end
 
+# DRIVER CODE
+
+george = Puppy.new
+
+george.fetch("stick")
+george.speak(3)
+george.roll_over
+george.dog_years(3)
+george.shake_hands(2)
