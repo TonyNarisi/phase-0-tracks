b3bd4f853578e76baca800e645942ab92cd4d91c
Tony Narisi <tonynarisi@gmail.com>
2016-06-08 22:05:42 -0400

    Pseudocode cup class

---
+++ ruby/solo.rb
@@ -0,0 +1,9 @@
+# Designing a "Cup" class
+# Every cup has three attributes:
+#   - size (in mL)
+#   - contents
+#   - color
+# Every cup has three methods:
+#   - pour out (removes contents, replacing with "none")
+#   - fill up (adds contents to cup with no contents)
+#   - ponder life (user looks at cup and wonders if it is half full or half empty)
