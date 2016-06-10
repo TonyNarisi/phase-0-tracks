235992f0c47d8bc33101d3157d9552f6dce48cf0
Tony Narisi <tonynarisi@gmail.com>
2016-06-09 20:14:41 -0400

    Add methods and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
 
+  def self.yell_happily(words)
+    words + "! Heck yes!"
+  end
 end
+
+# DRIVER CODE
+
+p Shout.yell_angrily("I can't believe that")
+p Shout.yell_happily("I can't believe that")
