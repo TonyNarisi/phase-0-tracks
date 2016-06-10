fa8f12be92cc3199fd4c4930b5cb83fbbb086b1c
Tony Narisi <tonynarisi@gmail.com>
2016-06-09 20:43:08 -0400

    Include shout module in classes and write driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,46 @@
+# Release 1
+
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "! Heck yes!"
+#   end
+# end
+
+# DRIVER CODE
+
+# p Shout.yell_angrily("I can't believe that")
+# p Shout.yell_happily("I can't believe that")
+
+# Release 3
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
     words + "! Heck yes!"
   end
 end
 
+class PersonDriving
+  include Shout
+end
+
+class SportsFan
+  include Shout
+end
+
 # DRIVER CODE
 
-p Shout.yell_angrily("I can't believe that")
-p Shout.yell_happily("I can't believe that")
+driver = PersonDriving.new
+fan = SportsFan.new
+
+p driver.yell_angrily("Excuse me, sir, I believe you cut me off")
+p driver.yell_happily("Oh boy, it looks like traffic is thinning")
+p fan.yell_angrily("Oh man, I wanted the other team to win")
+p fan.yell_happily("The team I like has more points than the other team")
