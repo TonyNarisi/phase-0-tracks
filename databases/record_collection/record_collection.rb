3aef8737869a08c86dc870a344978817931b110e
Tony Narisi <tonynarisi@gmail.com>
2016-06-23 20:49:56 -0400

    Initial commit

---
+++ databases/record_collection/record_collection.rb
@@ -0,0 +1,16 @@
+# Record Collection Manager Program
+
+# Require gems
+require 'sqlite3'
+
+# Create SQLite3 database
+db = SQLite3::Database.new("records.db")
+
+# Create a record_collection table if one does not exist
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS record_collection(
+    id INTEGER PRIMARY KEY,
+    artist_name VARCHAR(255),
+    album_name VARCHAR(255),
+    rating INT,
+    
